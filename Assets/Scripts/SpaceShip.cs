98aaa84aef39cce2dc129ccb9bc7e3335567f989
Antonio F <antoniofinisterrafreitas@gmail.com>
2021-03-30 09:16:01 +0100

    Initial commit

---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+    [SerializeField]
+    float velocity = 0.5f;
+
+    [SerializeField]
+    KeyCode LeftKey = KeyCode.LeftArrow;
+
+    [SerializeField]
+    KeyCode RightKey = KeyCode.RightArrow;
+
+
+    void Update()
+    {
+        if (Input.GetKey(LeftKey))
+        {
+            // x = x0 + v0 * (x,y,z) * t 
+            transform.position += velocity * Vector3.left * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(RightKey))
+        {
+            transform.position += velocity * Vector3.right * Time.deltaTime;
+        }
+
+        float camerawidth = Camera.main.orthographicSize + 1f;
+        float halfSpaceship = 0.3f;
+
+        // Clamp -> (value, min, max)
+        Vector3 positionAux = transform.position;
+        positionAux.x = Mathf.Clamp(transform.position.x,
+                        -camerawidth + halfSpaceship,
+                        camerawidth - halfSpaceship);
+        transform.position = positionAux;
+    }
+}
---
+++ Assets/Scripts/SpawnInvaders.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnInvaders : MonoBehaviour
+{
+    [SerializeField]
+        GameObject InvaderA;
+    [SerializeField]
+        GameObject InvaderB;
+    [SerializeField]
+        GameObject InvaderC;
+
+    [SerializeField]
+        int NInvaders = 0;   //number of Invaders per row
+    [SerializeField]
+        float InvadersY = 0; //height of Invaders A row (determines the height of Invaders B & C rows)
+
+        int NinvaderB;   //number of Invaders B
+        float InvaderBy; //height of Invader B row
+
+        int NinvaderC= 0;   //number of Invaders C
+        float InvaderCy= 0; //height of Invaders C row
+
+    [SerializeField]
+        float DistanceBTW = 0; //Distance between each row of invaders
+
+    private void Awake()
+    {
+        if (NInvaders < 9)
+        { 
+            float xMin = -((NInvaders / 2f) - 1/2f);
+            float x = xMin;
+
+                for (int i = 1; i <= NInvaders; i++)
+                {GameObject newinvader = Instantiate(InvaderA, transform);          //Instantiate: duplicates given gameobject in given position
+                newinvader.transform.position = new Vector3(x, InvadersY, 0);
+                x += 1f;}
+
+            NinvaderB = NInvaders;
+            NinvaderC = NInvaders;
+        }
+        else
+        {
+            InvadersExcess(); // if NInvaders > 9 no invader will be displayed
+        }
+
+        if (NinvaderB < 9)
+        {
+        InvaderBy = InvadersY + DistanceBTW;
+            float xMin = -((NinvaderB / 2f) - 1 / 2f);
+            float x = xMin;
+
+            for (int i = 1; i <= NinvaderB; i++)
+            {GameObject newinvader = Instantiate(InvaderB, transform);
+                newinvader.transform.position = new Vector3(x, InvaderBy, 0);
+                x += 1f;}
+        }
+        else
+        {
+            InvadersExcess();
+        }
+
+        if (NinvaderC < 9)
+        {
+            InvaderCy = InvaderBy + DistanceBTW;
+            float xMin = -((NinvaderC / 2f) - 1 / 2f);
+            float x = xMin;
+
+            for (int i = 1; i <= NinvaderC; i++)
+            {GameObject newinvader = Instantiate(InvaderC, transform);
+                newinvader.transform.position = new Vector3(x, InvaderCy, 0);
+                x += 1f;}
+        }
+        else
+        {
+            InvadersExcess();
+        }
+    }
+
+    void InvadersExcess()
+    {
+        Debug.Log("<N Invaders> must be lower than 9!");
+    }
+}
